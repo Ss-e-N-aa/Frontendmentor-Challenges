187284170b0dfef31ba31f091a0bcc2b05c01834
Zena Abodan <zena.abudan@gmail.com>
2025-02-02 14:57:53 +0300

    fixes

--- Social links/vite.config.js
+++ Social links/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Frontendmentor-Challenges/social-links/',
+  base: '/Frontendmentor-Challenges/',
   plugins: [react()],
 })
